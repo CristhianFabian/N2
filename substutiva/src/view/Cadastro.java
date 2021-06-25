435e92be5121471e321724cdfad59b1d468e4a34
Cristhian Fabian Rosa de Almeida <cristhian.fabianq@gmail.com>
2021-06-24 21:08:11 -0300

    ajustes na classe Cadastro

--- substutiva/src/view/Cadastro.java
+++ substutiva/src/view/Cadastro.java
@@ -14,18 +14,20 @@ public class Cadastro {
         List<Pessoa> listaPessoa = new ArrayList<>();
         
         while ( ! opcao.equalsIgnoreCase ("n")){
-            String texto = "Digite sua opção: \n" + 
+            String op1 = "Digite sua opção: \n" + 
                            "1 - Cadastrar Pessoa \n" + 
                            "2 - Incrementar Idade \n" + 
                            "3 - Listar Pessoa \n" + 
                            "4 - Sair ";
-            opcao = JOptionPane.showInputDialog(texto);
+            opcao = JOptionPane.showInputDialog(op1);
             
             switch(opcao){
                 case "1":
-                    String texto = "Digite a opção: \n" +
+                    String opp = "Digite a opção: \n" +
                                     "1 - Pessoa Fisica\n" +
                                     "2 - Pessoa Juridica\n";
+                    opp = JOptionPane.showInputDialog(opp);
+                    
             }
         
         }
